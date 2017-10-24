e2e28f1eae49152e8f3fe1692783d191bd76ade0
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-10-25 00:46:09 +0300

    feat: loaders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom'
-import {Container} from 'semantic-ui-react';
+import {Container, Loader} from 'semantic-ui-react';
 import Login from "./widget-login/Login";
 import SignUp from "./widget-sign-up/SignUp";
 import Home from "./home/Home";
@@ -17,7 +17,8 @@ class App extends Component {
     this.state = {
       user: undefined,
       canRedirectHome: false,
-      redirectHome: false
+      redirectHome: false,
+      isLoading: true
     };
 
     // if (this.props.location.pathname !== this.homePath) {
@@ -25,34 +26,45 @@ class App extends Component {
     // }
     //
     firebase.auth().onAuthStateChanged(user => {
-      if (user) {
+      if (user !== null) {
         this.setState({user: user, redirectHome: true});
+      } else {
+        this.setState({user: "NO_USER"});
       }
+      this.setState({isLoading: false});
     });
   }
 
   render() {
-    const {redirectHome} = this.state;
+    const {redirectHome, isLoading} = this.state;
 
-    return (
-      <div>
-        <Router>
-          <div>
-            <Route path="/home" component={Home}/>
-            <Container text className="padding-top--lg">
-              <Route exact path="/" render={() => (
-                redirectHome ? (
-                  <Redirect to={this.homePath}/>
-                ) : (
-                  <Login />
-                )
-              )}/>
-              <Route path="/sign-up" component={SignUp}/>
-            </Container>
-          </div>
-        </Router>
-      </div>
-    );
+    if (isLoading) {
+      return (
+        <div>
+          <Loader active className="padding-top--lg" inline='centered'/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Router>
+            <div>
+              <Route path="/home" component={Home}/>
+              <Container text className="padding-top--lg">
+                <Route exact path="/" render={() => (
+                  redirectHome ? (
+                    <Redirect to={this.homePath}/>
+                  ) : (
+                    <Login user={this.state.user}/>
+                  )
+                )}/>
+                <Route path="/sign-up" component={SignUp}/>
+              </Container>
+            </div>
+          </Router>
+        </div>
+      );
+    }
   }
 }
 
--- src/widget-login/Login.js
+++ src/widget-login/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Router, Redirect, Link, withRouter} from 'react-router-dom'
-import {Message, Button, Form, Icon, Divider} from 'semantic-ui-react';
+import {Message, Button, Form, Icon, Divider, Loader} from 'semantic-ui-react';
 import firebase from '../firebase';
 
 class Login extends Component {
@@ -12,69 +12,90 @@ class Login extends Component {
   loginFormPasswordPlaceholder = "Password";
   loginMessageTitle = "Not a member?";
   loginMessageSignUp = "Sign up";
-  state = {
-    redirectToHome: false
-  };
 
-  onLogin = () => {
-    this.setState({ redirectToHome: true })
-  };
-
-  signInWithGoogle = () => {
-    let provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithRedirect(provider).then(() => {
-      firebase.auth().getRedirectResult().then(result => {
-        if (result.credential) {
-          const {accessToken} = result.credential;
-        }
-        const {user} = result;
-        //TODO: dispatch user here
-        if(user) {
-          this.setState({redirectToHome: true})
-        }
-      }).catch(function (error) {
-        const {code, message, email, credential} = error;
-      });
-    });
-  };
 
   constructor(props) {
     super(props);
     this.state = {
-      user: undefined
+      redirectToHome: false,
+      isLoading: true
     };
   }
 
+  componentDidMount(){
+    if(this.props.user === 'NO_USER'){
+      this.setState({isLoading: false});
+    }
+  }
+
+  onLogin = () => {
+    this.setState({redirectToHome: true})
+  };
+
+  signInWithGoogle = () => {
+    this.setState({isLoading: true});
+    let provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth()
+      .signInWithRedirect(provider)
+      .then(() => {
+        firebase.auth().getRedirectResult()
+          .then(result => {
+            const {user} = result;
+            if (user) {
+              this.setState({redirectToHome: true})
+            }
+            this.setState({isLoading: false});
+          })
+          .catch((error) => {
+            const {code, message, email, credential} = error;
+            this.setState({isLoading: false});
+          })
+      });
+  };
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
   render() {
-    const {redirectToHome} = this.state;
+    const {redirectToHome, isLoading} = this.state;
 
     if (redirectToHome) return <Redirect to="/home/main"/>;
 
-    return (
-      <div>
-        <h1>{this.loginTitle}</h1>
-        <Form size="big">
-          <Form.Field>
-            <input type="email" placeholder={this.loginFormEmailPlaceholder}/>
-          </Form.Field>
-          <Form.Field>
-            <input type="password" placeholder={this.loginFormPasswordPlaceholder}/>
-          </Form.Field>
-          <Button fluid size="big" color="violet" type="submit"
-                  onClick={this.onLogin}>{this.loginFormSignInButtonLabel}
+    if (isLoading) {
+      return (
+        <div>
+          <Loader active className="padding-top--lg" inline='centered'/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>{this.loginTitle}</h1>
+          <Form size="big">
+            <Form.Field>
+              <input type="email" placeholder={this.loginFormEmailPlaceholder}/>
+            </Form.Field>
+            <Form.Field>
+              <input type="password" placeholder={this.loginFormPasswordPlaceholder}/>
+            </Form.Field>
+            <Button fluid size="big" color="violet" type="submit"
+                    onClick={this.onLogin}>{this.loginFormSignInButtonLabel}
+            </Button>
+          </Form>
+          <Message>
+            <Icon name='help'/>
+            {this.loginMessageTitle}&nbsp;<Link to="/sign-up">{this.loginMessageSignUp}</Link>
+          </Message>
+          <Divider horizontal>Or</Divider>
+          <Button fluid size="big" color='google plus'
+                  onClick={this.signInWithGoogle}>
+            <Icon name='google plus'/> Sign in with Google
           </Button>
-        </Form>
-        <Message>
-          <Icon name='help'/>
-          {this.loginMessageTitle}&nbsp;<Link to="/sign-up">{this.loginMessageSignUp}</Link>
-        </Message>
-        <Divider horizontal>Or</Divider>
-        <Button fluid size="big" color='google plus'
-                onClick={this.signInWithGoogle}>
-          <Icon name='google plus' /> Sign in with Google
-        </Button>
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
 
