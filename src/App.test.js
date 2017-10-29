f8e934b1442a9e0aa99c1a332fb476279225ac14
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-10-29 19:17:49 +0200

    test: first passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createStore } from 'redux'
+import moodyStore from './store/reducers'
+import App from './App'
+
+configure({ adapter: new Adapter() });
+
+const store = createStore(moodyStore);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App store={store} />);
 });
